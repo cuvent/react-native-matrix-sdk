804c0a90de6f229ca846e3cd64d521e212f74d51
Hanno Gödecke <hgoedecke@cuvent.com>
2020-03-19 10:22:18 +0100

    Implemented configuration, login, starting session for android

--- android/src/main/java/com/reactlibrary/MatrixSdkModule.java
+++ android/src/main/java/com/reactlibrary/MatrixSdkModule.java
@@ -1,17 +1,18 @@
 package com.reactlibrary;
 
 import android.net.Uri;
-import android.util.Log;
 
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
+import org.json.JSONException;
 import org.matrix.androidsdk.HomeServerConnectionConfig;
 import org.matrix.androidsdk.MXDataHandler;
 import org.matrix.androidsdk.MXSession;
 import org.matrix.androidsdk.core.callback.SimpleApiCallback;
+import org.matrix.androidsdk.core.model.MatrixError;
 import org.matrix.androidsdk.data.store.MXMemoryStore;
 import org.matrix.androidsdk.rest.client.LoginRestClient;
 import org.matrix.androidsdk.rest.model.login.Credentials;
@@ -22,6 +23,10 @@ public class MatrixSdkModule extends ReactContextBaseJavaModule {
 
     private HomeServerConnectionConfig hsConfig;
     private MXSession mxSession;
+
+    private static final String E_MATRIX_ERROR = "E_MATRIX_ERROR";
+    private static final String E_NETWORK_ERROR = "E_NETWORK_ERROR";
+    private static final String E_UNEXCPECTED_ERROR = "E_UNKNOWN_ERROR";
 
 
     public MatrixSdkModule(ReactApplicationContext reactContext) {
@@ -39,25 +44,55 @@ public class MatrixSdkModule extends ReactContextBaseJavaModule {
         hsConfig = new HomeServerConnectionConfig.Builder()
                 .withHomeServerUri(Uri.parse(matrixServerUrl))
                 .build();
-        Log.i(TAG, "HomeServer config created for matrix server " + matrixServerUrl);
     }
 
 
     @ReactMethod
-    public void login(String username, String password, final Callback callback) {
+    public void login(String username, String password, Promise promise) {
         new LoginRestClient(hsConfig).loginWithUser(username, password, new SimpleApiCallback<Credentials>() {
             @Override
             public void onSuccess(Credentials info) {
-                callback.invoke(info.toString());
+                try {
+                    hsConfig.setCredentials(info);
+                    promise.resolve(info.toJson().toString());
+                } catch (JSONException e) {
+                    promise.reject(E_UNEXCPECTED_ERROR, "{\"error\": \"Couldn't parse JSON response\"}");
+                }
+            }
+
+            @Override
+            public void onMatrixError(MatrixError e) {
+                super.onMatrixError(e);
+                promise.reject(E_MATRIX_ERROR, "{\"error\": \""+e.getMessage()+"\"}");
+            }
+
+            @Override
+            public void onNetworkError(Exception e) {
+                super.onNetworkError(e);
+                promise.reject(E_NETWORK_ERROR,"{\"error\": \""+e.getMessage()+"\"}");
+            }
+
+            @Override
+            public void onUnexpectedError(Exception e) {
+                super.onUnexpectedError(e);
+                promise.reject(E_UNEXCPECTED_ERROR, "{\"error\": \""+e.getMessage()+"\"}");
             }
         });
     }
 
     @ReactMethod
-    public void startSession(Credentials credentials) {
-        mxSession = new MXSession.Builder(hsConfig,
-                new MXDataHandler(new MXMemoryStore(), credentials),
-                getReactApplicationContext())
+    public void startSession(Promise promise) {
+        mxSession = new MXSession.Builder(
+                hsConfig,
+                new MXDataHandler(
+                        // TODO: uses MemoryStore, FileStore maybe better for persistence? (caused issues when trying to use)
+                        new MXMemoryStore(
+                                hsConfig.getCredentials(),
+                                reactContext.getApplicationContext()
+                        ),
+                        hsConfig.getCredentials()),
+                reactContext.getApplicationContext())
                 .build();
+        promise.resolve(mxSession.isAlive());
     }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,22 @@
 declare module 'react-native-matrix-sdk' {
+
+  export interface Credentials {
+    user_id: string;
+    home_server: string;
+    access_token: string;
+    refresh_token: string | undefined;
+    device_id: string;
+  }
+
+  export interface Error {
+    error: string;
+  }
+
   export interface MatrixSdkStatic {
     configure(host: string): void;
+    // TODO: actually credentials are returned as string, and not as Credentials Type
+    login(username: string, password: string): Promise<Credentials>;
+    startSession(): Promise<boolean>;
   }
 
   const MatrixSdk: MatrixSdkStatic;
